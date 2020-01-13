34c3579e0a0f755d0f3cb9727657450bc75d5fe7
Sincy Chacko <sincy511@gmail.com>
2020-01-14 10:35:09 +1100

    Initial Commit

---
+++ FlashChatUdemy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashChatUdemy
+//
+//  Created by SINCY on 14/01/20.
+//  Copyright © 2020 SINCY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
